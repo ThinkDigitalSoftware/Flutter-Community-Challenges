09eea29b4d9de65f6ba99d2a68f3fad436f1d83d
GroovinChip <groovinchip@gmail.com>
2018-08-29 16:48:02 -0400

    remove boilerplate, split out main views into own widget out of main.dart

---
+++ lib/mainViews.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MainViews extends StatefulWidget {
+  MainViews({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MainViewsState createState() => _MainViewsState();
+}
+
+class _MainViewsState extends State<MainViews> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+
+      ),
+    );
+  }
+}
